b6159734aa483a52187f2d1538537ee53ccfaa80
Gary <Gary@Garys-MacBook-Pro.local>
2016-06-25 17:23:40 +0800

    remove outputFile[][]

--- train.cpp
+++ train.cpp
@@ -11,43 +11,22 @@ void initModel( Model&, const char* );
 void merge( WordsDistrib&, WordsDistrib& );
 vector< pair< int, prob > > transform( WordsDistrib& );
 
-char outputFile[ ][ 21 ] = { "models/model_00.mod",
-                             "models/model_01.mod",
-                             "models/model_02.mod",
-                             "models/model_03.mod",
-                             "models/model_04.mod",
-                             "models/model_05.mod",
-                             "models/model_06.mod",
-                             "models/model_07.mod",
-                             "models/model_08.mod",
-                             "models/model_09.mod",
-                             "models/model_10.mod",
-                             "models/model_11.mod",
-                             "models/model_12.mod",
-                             "models/model_13.mod",
-                             "models/model_14.mod",
-                             "models/model_15.mod",
-                             "models/model_16.mod",
-                             "models/model_17.mod",
-                             "models/model_18.mod",
-                             "models/model_19.mod" };
-
 int main() {
   FILE* fin = fopen( "filelist_train/filelist.list", "r" );
 
   Model m;
   int index = 0;
-  char list[ 100 ];
+  char list[ 100 ], fname[ 100 ];
   while ( fscanf( fin, "%s", list ) != EOF ) { 
     initModel( m, list );
-    m.write( outputFile[ index++ ] );
+    sprintf( fname, "models/model_%d.mod", index++ );
+    m.write( fname );
   }
 
   fclose( fin );
 }
 
 void initModel( Model& m, const char* list ) {
-  cout << list << endl;
   FILE* fin = fopen( list, "r" ); 
 
   char file[ 100 ];
